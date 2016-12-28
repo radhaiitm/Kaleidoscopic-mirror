4ab2490e337d0913379163a769aa49d8ce68d9b8
radhaiitm <radhakrishna8598@gmail.com>
2016-12-28 11:45:30 +0530

    First commit

---
+++ Assets/RSUnityToolkit/Assets/Scripts/ScanRelease.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScanRelease : MonoBehaviour {
+	
+	public GameObject 	scanner;
+	public GameObject	IRfeed;
+	//public Light		IRlight;
+	public Camera		myCam;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		
+		if(Input.GetKey(KeyCode.F3))
+			Release();
+		
+	}
+	
+	void Release(){
+		
+		myCam.GetComponent<Animation>().Play();
+		//IRlight.animation.Play();
+		scanner.GetComponent<Animation>().Play();
+		IRfeed.GetComponent<Animation>().Play();
+	}
+}
